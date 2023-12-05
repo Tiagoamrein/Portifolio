44081405bfc811630d79724eeaceef1604492697
tiago <tiagoamrein@gmail.com>
2023-12-04 22:07:37 -0300

    responsive header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,15 +4,15 @@ export default function Header(){
 
   return(
     
-    <div className='flex justify-between items-center  py-4 w-screen h-full  '>
-    <h1 className="text-xs font-bold">THSCsystem</h1>
-    <div className='flex gap-2 justify-center text-ssm'>
+    <div className='flex justify-between items-center px-2 py-4 w-screen sm:px-4 '>
+    <h1 className="text-xs font-bold min-[360px]:text-sm sm:text-2xl">THSCsystem</h1>
+    <div className='flex gap-1 justify-center text-ssm  sm:text-base gap-2'>
       <button className='hover:text-sky-900  rounded-lg'>Home</button>
       <button className='hover:text-sky-900  rounded-lg'>Projetos</button>
       <button className='hover:text-sky-900  rounded-lg'>Soluções</button>
       <button className='hover:text-sky-900 rounded-lg'>Sobre</button>
     </div>
-    <button className='  bg-sky-700 text-ssm px-1 py-1 font-semibold text-slate-200  border-2 border-transparent  rounded-lg hover:bg-white hover:border-2 hover:border-slate-400 hover:text-black  '>Contato</button>
+    <button className='  bg-sky-700 text-ssm px-1 font-semibold text-slate-200  border-2 border-transparent  rounded-lg hover:bg-white hover:border-2 hover:border-slate-400 hover:text-black min-[360px]:text-xs sm:text-base py-1'>Contato</button>
   </div>
   
   )
