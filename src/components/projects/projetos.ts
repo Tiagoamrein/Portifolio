f5fbee58a810cbbb15caca409f7cf015ebab878a
tiago <tiagoamrein@gmail.com>
2024-01-06 17:36:02 -0300

    update projects

--- src/components/projects/projetos.ts
+++ src/components/projects/projetos.ts
@@ -7,15 +7,15 @@
 
 export const projetos: Projeto[] = [
   {
-    image: '/notesapp.png',
-    title: 'Rocketnotes',
-    description: 'Projeto desenvolvido para gerenciamento de filmes',
-    link: 'https://rocketseat-note.netlify.app/'
+    image: '/todo-list.png',
+    title: 'Todo list',
+    description: 'Aplicativo web e mobile de lista de tarefas',
+    link: 'https://todo-list-phi-sandy.vercel.app/'
   },
   {
     image: '/204163350-1d4c8a96-f68a-4eb0-aa8e-5343e2681749.png',
-    title: 'Projeto 2',
-    description: 'Descrição do Projeto 2...',
+    title: 'Food explorer',
+    description: 'Sistema de cardapio e pedidos para restaurante',
     link: 'https://food-explorer-ts.netlify.app/'
   },
   {
@@ -24,23 +24,6 @@ export const projetos: Projeto[] = [
     description: 'Projeto desenvolvido para gerenciamento de filmes',
     link: 'https://rocketseat-note.netlify.app/'
   },
-  {
-    image: '/204163350-1d4c8a96-f68a-4eb0-aa8e-5343e2681749.png',
-    title: 'Projeto 2',
-    description: 'Descrição do Projeto 2...',
-    link: 'https://food-explorer-ts.netlify.app/'
-  },
-  {
-    image: '/notesapp.png',
-    title: 'Rocketnotes',
-    description: 'Projeto desenvolvido para gerenciamento de filmes',
-    link: 'https://rocketseat-note.netlify.app/'
-  },
-  {
-    image: '/204163350-1d4c8a96-f68a-4eb0-aa8e-5343e2681749.png',
-    title: 'Projeto 2',
-    description: 'Descrição do Projeto 2...',
-    link: 'https://food-explorer-ts.netlify.app/'
-  },
+ 
   
 ];
